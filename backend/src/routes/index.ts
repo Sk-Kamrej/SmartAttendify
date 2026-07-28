345e5b98333ce7d66b85b02a435f42cfa7250cd6
SK Kamrej <skkamrej786@gmail.com>
2026-07-28 17:25:35 +0530

    backend updated

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
+
 import authRoutes from "../modules/auth/auth.routes.js";
+import collegeRoutes from "../modules/college/college.routes.js";
 
 const router = Router();
 
 router.use("/auth", authRoutes);
+router.use("/colleges", collegeRoutes);
 
 export default router;
