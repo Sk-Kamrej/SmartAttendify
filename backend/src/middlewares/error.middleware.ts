39dd802bab2bda8ac615d7eb527388d8d5321ba6
SK Kamrej <skkamrej786@gmail.com>
2026-07-28 15:43:34 +0530

    src: middlewares updated

---
+++ backend/src/middlewares/error.middleware.ts
@@ -0,0 +1,26 @@
+import { Request, Response, NextFunction } from "express";
+import ApiError from "../utils/ApiError.js";
+
+const errorMiddleware = (
+  err: unknown,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  let statusCode = 500;
+  let message = "Internal Server Error";
+
+  if (err instanceof ApiError) {
+    statusCode = err.statusCode;
+    message = err.message;
+  } else if (err instanceof Error) {
+    message = err.message;
+  }
+
+  res.status(statusCode).json({
+    success: false,
+    message,
+  });
+};
+
+export default errorMiddleware;
